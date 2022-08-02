7adc35e9000dc574336f2b907a245a61ff40f27c
Z34L234 <nestyu66@gmail.com>
2022-08-02 06:41:14 +0100

    Added html and javascript file

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+let variablename = "value"  const origin = "Enugu"
+var variablename = "value"
+const variablename = "value"
+var a = 2
+let b = 3
+var a = 1
+let b = 0
+*/
+   
+   /*let array = [1,2,3,4,5]
+   array[2]='wow, I did it'
+   console.log (array)*/
+   
+   console.log (true && false)
+   
---
+++ solution.js
@@ -0,0 +1,9 @@
+const cat = {
+    name: 'Betsy',
+    age: 1,
+    gender: 'female',
+    height: '5.4'
+}
+let lastitemposition = cat.height = 2.3;
+
+console.log(cat);
